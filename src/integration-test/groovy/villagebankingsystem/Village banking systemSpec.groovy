b2cae603f0b52bdc9ec12debf68485661f6128f3
Praise Mulenga <praisemulengas@gmail.com>
2024-05-04 08:40:40 +0200

    Initial commit

---
+++ src/integration-test/groovy/villagebankingsystem/Village banking systemSpec.groovy
@@ -0,0 +1,22 @@
+package villagebankingsystem
+import grails.gorm.transactions.Rollback
+import grails.testing.mixin.integration.Integration
+
+import geb.spock.*
+
+/**
+ * See https://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+@Rollback
+class Village banking systemSpec extends GebSpec {
+
+    void "test something"() {
+        when:"The home page is visited"
+            go '/'
+
+        then:"The title is correct"
+            title == "Welcome to Grails"
+    }
+
+}
